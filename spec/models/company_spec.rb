226937751a02c686a605a17df652fb71761d7a36
Igor C. Batista <igor.batista@gmail.com>
2023-09-28 13:04:32 -0300

    create model company

---
+++ spec/models/company_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Company, type: :model do
+  subject(:company) { described_class.new(name: 'Anything') }
+
+  it { is_expected.to respond_to(:name) }
+
+  it 'is valid with valid attributes' do
+    expect(company.valid?).to be(true)
+  end
+
+  it 'is not valid without a name' do
+    company.name = nil
+    expect(company.valid?).to be(false)
+  end
+
+  it 'has a unique name' do
+    described_class.create!(name: 'Example')
+    company_duplicate = described_class.new(name: 'Example')
+    expect(company_duplicate.valid?).not_to be(true)
+    expect(company_duplicate.errors[:name]).to include('has already been taken')
+  end
+end
