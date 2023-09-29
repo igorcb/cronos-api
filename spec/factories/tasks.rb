2a047ac600ec2aef380d137fc5b51c762196cafc
Igor C. Batista <igor.batista@gmail.com>
2023-09-29 19:48:35 -0300

    fix: rubocop tasks factories

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :task do
     company { nil }
     software { nil }
-    code { "MyString" }
-    name { "MyString" }
-    description { "MyText" }
-    date_opened { "2023-09-29" }
+    code { 'MyString' }
+    name { 'MyString' }
+    description { 'MyText' }
+    date_opened { '2023-09-29' }
     status { 1 }
-    date_delivered { "2023-09-29" }
-    observation { "MyText" }
+    date_delivered { '2023-09-29' }
+    observation { 'MyText' }
   end
 end
