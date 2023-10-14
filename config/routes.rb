4036c69ab4be2bbeefd4c081a67079a60a4997f4
Igor C. Batista <igor.batista@gmail.com>
2023-10-14 10:05:15 -0300

    create routes software by company id (#11)

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # get '/tasks', to: 'tasks#index'
   resources :tasks
 
+  get '/companies/:company_id/softwares', to: 'welcome#softwares_by_company_id'
   get '/softwares', to: 'welcome#softwares'
   get '/companies', to: 'welcome#companies'
 
