b0d6c7a51c572ede45349426960c7611f48dffad
Igor C. Batista <igor.batista@gmail.com>
2023-12-04 19:30:44 -0300

    fix: find_update (#29)
    * fix: find_update
    
    * fix: find_update and spec

--- app/services/upload_service.rb
+++ app/services/upload_service.rb
@@ -9,6 +9,7 @@ class UploadService
   def call
     excel = Roo::Excelx.new(@file_path)
     count_lines = 0
+
     excel.each_with_index do |row, index|
       next if index.zero? || row[0].blank?
 
@@ -23,6 +24,7 @@ class UploadService
 
       @date_opened = row[1]
       @date_start = @date_opened
+
       @hour_start = row[2]
       @date_end = @date_opened
       @hour_end = row[3]
